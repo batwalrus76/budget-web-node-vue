3518120b0552a8a5eb3aa02a6ef49d58a4a6adc6
batwalrus76 <chancebatwalrus@gmail.com>
2019-11-04 12:57:53 -0500

    adding source files

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">budget</h1>
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+
+export default {
+  name: "home",
+  components: {
+  },
+  mounted() {
+  },
+  computed: {
+    ...mapGetters(["isAuthenticated"])
+  }
+};
+</script>
---
+++ src/views/MainBudget.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <div class="home-global"><RwvArticleList type="all" /></div> -->
+  Main Budget!
+</template>
+<script>
+
+export default {
+  name: "rwv-main-budget",
+  components: {
+
+  }
+};
+</script>

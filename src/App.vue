d4e012bd3f4985e208cdf3c629e380e413c374f1
batwalrus76 <chancebatwalrus@gmail.com>
2019-11-06 09:49:59 -0500

    Added navbar and cards, its a start

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-  <div id="app">
-  <b-navbar type="dark" class="nav-background">
-
-    <b-navbar-brand href="#">Budget</b-navbar-brand>
-
-</b-navbar>
+    <div id="app">
+        <b-navbar type="dark" class="nav-background">
+            <b-navbar-brand href="#">Budget</b-navbar-brand>
+            <div class="px-4" style="color: #fff">Start Date: {{currentStartDate}} </div>
+            <div class="px-4" style="color: #fff">End Date: {{currentEndDate}} </div>
+            <b-button-group class="px-4">
+                <b-button variant="danger">Previous Week</b-button>
+                <b-button variant="success">Next Week</b-button>
+            </b-button-group>
+        </b-navbar>
 
     <b-container>
-        <b-row class="mt-5">
-            <b-form-group id="input-group-1" label="Start Date:" label-for="input-1">
-                <b-form-input id="input-1" v-model="this.currentStartDate" placeholder="Current Start Date"></b-form-input>
-            </b-form-group>
-            <b-form-group id="input-group-2" label="End Date:" label-for="input-2">
-                <b-form-input id="input-2" v-model="this.currentEndDate" placeholder="Current End Date"></b-form-input>
-            </b-form-group>
-        </b-row>
+        <b-card-group style="max-width:100%">
+            <b-card
+                tag="article"
+                style="max-width: 75%"
+                header="Current Week Budget"
+            >
+                <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+                </b-card-text>
+            </b-card>
+            <b-card
+                header="Accounts"
+                tag="article"
+                style="max-width: 25%"
+            >
+                <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+                </b-card-text>
+            </b-card>
+        </b-card-group>
+        <b-card-group style="max-width:100%">
+
+        </b-card-group>    
     </b-container>
   </div>
 </template>
@@ -28,15 +47,32 @@ export default {
   },
   data() {
     return {
-      show: true,
-      currentStartDate: Date.now().toDateString(),
-      currentEndDate: "11-10-2019"
+        currentStartDate: this.formatDate(this.getPreviousFriday(new Date())),
+        currentEndDate: this.formatDate(this.getNextThursday(new Date()))
     }
   },
   methods: {
-    handleClick() {
-      this.showDismissibleAlert = true
-    }
+        getPreviousFriday(currentDate){
+            currentDate.setDate(currentDate.getDate() + (6 - currentDate.getDay() - 1) - 7);
+            return currentDate;
+        },
+        getNextThursday(currentDate) {
+            currentDate.setDate(currentDate.getDate() + (4 - currentDate.getDay()));
+            return currentDate;
+        },
+        formatDate(date) {
+            var d = new Date(date),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+
+            if (month.length < 2) 
+                month = '0' + month;
+            if (day.length < 2) 
+                day = '0' + day;
+
+            return [month, day, year].join('-');
+        }
   }
 }
 </script>
